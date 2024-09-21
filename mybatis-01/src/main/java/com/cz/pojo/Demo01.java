edb4daf8416fd7d51c4e7ea3bf1cee6e6822da0d
cz <209059391@qq.com>
2024-09-21 15:54:54 +0800

    init

--- mybatis-01/src/main/java/com/cz/pojo/Demo01.java
+++ mybatis-01/src/main/java/com/cz/pojo/Demo01.java
@@ -3,6 +3,7 @@ package com.cz.pojo;
 public class Demo01 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("This is my first Java program.");
     }
 
 }
