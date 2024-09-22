dcf0ffdcf05f966aa3bdf0b848dc37c4ed79b0b4
cz <209059391@qq.com>
2024-09-22 09:34:01 +0800

    init

--- mybatis-01/src/test/java/MyTest.java
+++ mybatis-01/src/test/java/MyTest.java
@@ -1,5 +1,23 @@
-public class MyTest {
-    public static void main(String[] args) {
+import com.cz.Dao.UserDao;
+import com.cz.pojo.User;
+import com.cz.utils.Mybatisutil;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
 
+import java.util.List;
+
+public class MyTest {
+    @Test
+    public void test() {
+        //获取SqlSession对象
+        SqlSession sqlSession = Mybatisutil.getSqlSession();
+        //执行SQL语句
+        UserDao mapper = sqlSession.getMapper(UserDao.class);
+        List<User> allUsers = mapper.getAllUsers();
+        for (User allUser : allUsers) {
+            System.out.println(allUser);
+        }
+        //关闭SqlSession对象
+        sqlSession.close();
     }
 }
